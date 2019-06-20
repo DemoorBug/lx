7dc8fc191299af86e5570b7daef0b968862b2313
debug <18161975696@163.com>
2019-06-21 00:25:09 +0800

    new add TypeScript

---
+++ typescript/greeter.js
@@ -0,0 +1,9 @@
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+// let c: Color = Color[2]
+var c = Color[2];
+console.log(c);
---
+++ typescript/greeter.ts
@@ -0,0 +1,10 @@
+enum Color {
+  Red,
+  Green,
+  Blue
+}
+
+// let c: Color = Color[2]
+let c: string = Color[2]
+
+console.log(c)
